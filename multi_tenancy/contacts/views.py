5def046a2bf1cac509afd2d71e5b9183c1596793
Ramu Kodandapuram <ramukodandapuram@gmail.com>
2018-05-04 20:15:05 +0530

    Update views.py

--- multi_tenancy/contacts/views.py
+++ multi_tenancy/contacts/views.py
@@ -15,18 +15,8 @@ class ContactSerializer(serializers.ModelSerializer):
 # Create your views here.
 
 class ContactViewSet(APIView):
-	#queryset = Contact.objects.all()
+	queryset = Contact.objects.all()
 	
-	#serializer_class = ContactSerializer
+	serializer_class = ContactSerializer
 
-	def get_object(self,pk):
-		try:
-			pdb.set_trace()
-			return Contact.objects.get(pk=pk)
-		except Contact.DoesNotExist:
-			raise Http404
-
-	def get(self,request,pk,format=None):
-		contatcts=self.get_object(pk)
-		serializer=ContactSerializer(contatcts)
-		return Response(serializer.data)
+	
